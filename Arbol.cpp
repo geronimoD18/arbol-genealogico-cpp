cbbee7f2c9c1b9e6ae5163a06452119b938f0cb7
JDevshington <202010314@itla.edu.do>
2023-07-31 13:48:24 -0400

    Eliminacion del mandato de tarea

--- Arbol.cpp
+++ Arbol.cpp
@@ -71,39 +71,3 @@ void Nodo::AgregarHijo(Nodo *hijo)
 }
 
 // modification by Geronimo on Monday, July 31, 2023
-
-/*
-
-hey bro, tengo una tarea en la cual tengo que completar un codigo para realizar un arbol genealogico. Te proporcionare el codigo fuente mas abajo. Mira el mandato:
-
-"Tarea 6: Árbol Genealógico
-Haga una aplicación que permita manejar el árbol genealógico de una familia.
-Esta aplicación permitirá agregar así como desplegar el árbol completo.
-
-- Figura 6.1. Menú principal
-Arbol Genealogico
-
-1- Crear árbol genealógico
-2- Desplegar árbol genealógico
-3- Salir
-__________________________________
-
-- Figura 6.2 Inserción de datos al árbol
-Ingrese el nombre de la persona: Carlos
-Digite la cantidad de hijos de Carlos: 2
-Digite el nombre del hijo #1 de Carlos: Pedro
-Digite el nombre del hijo #2 de Carlos: Juan
-Digite la cantidad de hijos de Pedro: 0
-Digite la cantidad de hijos de Juan: 1
-Digite el nombre del hijo #1 de Juan: Lucas
-Digite la cantidad de hijos de Lucas: 0
-____________________________________________
-
-- Figura 6.3. Impresión del árbol genealógico
-Carlos es padre de: Pedro y Juan
-Pedro no tiene hijos
-Juan es padre de Lucas
-Lucas no tiene hijos
-"
-
-*/
